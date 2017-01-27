88b0e6361c6db953cc2466f393c519c9d4b61911
ShaneQi <qizengtai@gmail.com>
2017-01-27 09:18:52 -0800

    Initial Commit

---
+++ IGListKitCellSizeCrashing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGListKitCellSizeCrashing
+//
+//  Created by Shane Qi on 1/27/17.
+//  Copyright © 2017 Shane Qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
