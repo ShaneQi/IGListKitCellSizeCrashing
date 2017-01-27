7c39d53bd8e5a6b64438591122e07a7b3ecf20a5
ShaneQi <qizengtai@gmail.com>
2017-01-27 11:30:00 -0800

    Simplify cells.

--- IGListKitCellSizeCrashing/Cells.swift
+++ IGListKitCellSizeCrashing/Cells.swift
@@ -11,33 +11,11 @@ import UIKit
 class MessageCell: UICollectionViewCell {
 
 	@IBOutlet var textLabel: UILabel!
-	
-	override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
-		setNeedsLayout()
-		layoutIfNeeded()
-		let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
-		var newFrame = layoutAttributes.frame
-		// note: don't change the width
-		newFrame.size.height = ceil(size.height)
-		layoutAttributes.frame = newFrame
-		return layoutAttributes
-	}
 
 }
 
 class PhotoCell: UICollectionViewCell {
 
 	@IBOutlet var imageView: UIImageView!
-	
-	override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
-		setNeedsLayout()
-		layoutIfNeeded()
-		let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
-		var newFrame = layoutAttributes.frame
-		// note: don't change the width
-		newFrame.size.height = ceil(size.height)
-		layoutAttributes.frame = newFrame
-		return layoutAttributes
-	}
 	
 }
